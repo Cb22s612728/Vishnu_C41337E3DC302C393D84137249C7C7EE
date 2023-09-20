e1fe0b70c9a44ce810596aadbf3dd817ca939627
Cb22s612728 <145107250+Cb22s612728@users.noreply.github.com>
2023-09-20 12:33:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+"""
+Challenge 1.1
+Factorial of a number using recursion
+"""
+def fact(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact(n-1)
+
+n= int(input("Enater a number: "))
+print('Factorial of {} is {}'.format(n,fact(n)))121212 
